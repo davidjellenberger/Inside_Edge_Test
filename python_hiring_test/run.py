904b655553d0c5bd3a878f907e234adb764b115a
David Elleneberger <ellenb1@stolaf.edu>
2018-04-09 10:46:15 -0500

    Fixed rounding, it was a silly mistake where I was rounding each value right as it was calculated, instead moved the rounding to the end

--- python_hiring_test/run.py
+++ python_hiring_test/run.py
@@ -36,12 +36,12 @@ def main():
     # # # 
 
     # Grab hitter's batting avg against righties
-    avg_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['H'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['AB'].sum())).round(3)
+    avg_against_rhp = (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['H'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['AB'].sum())
     # I'm not sure who team 142 is, but they really could use more time in the cages...
     # Anyway, now against lefties
-    avg_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['H'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['AB'].sum())).round(3)
+    avg_against_lhp = (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['H'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['AB'].sum())
 
     # Now we can create the intermediary dataframes from these series
     avg_against_rhp_df = avg_against_rhp.to_frame(name='Value')
@@ -60,22 +60,22 @@ def main():
     # OBP vs right handed
     # Apologizing in advance for the wall of brackets 
     # Find batters's OBP against righties - calculated as (hits + hbp + walks) / (total plate appearances)
-    obp_against_rhp = (((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['H'].sum()) \
+    obp_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['H'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['AB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['SF'].sum()))
 
     # Now for a batter's OBP against lefties
-    obp_against_lhp = (((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['H'].sum()) \
+    obp_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['H'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['AB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['SF'].sum()))
 
 	# Create dataframes
     obp_against_rhp_df = obp_against_rhp.to_frame(name='Value')
@@ -93,12 +93,12 @@ def main():
 
 
     # Calculate slugging percentage against rhp 
-    slg_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['TB'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['AB'].sum())).round(3)
+    slg_against_rhp = (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['TB'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterId')['AB'].sum())
 
     # Calculate slugging percentage against lhp 
-    slg_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['TB'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['AB'].sum())).round(3)
+    slg_against_lhp = (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['TB'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterId')['AB'].sum())
 
     # Dataframes
     slg_against_rhp_df = slg_against_rhp.to_frame(name='Value')
@@ -115,10 +115,10 @@ def main():
 
 
     # Calculate OPS against righties
-    ops_against_rhp = (obp_against_rhp + slg_against_rhp).round(3)
+    ops_against_rhp = (obp_against_rhp + slg_against_rhp)
 
     # Now against lefties
-    ops_against_lhp = (obp_against_lhp + slg_against_lhp).round(3)
+    ops_against_lhp = (obp_against_lhp + slg_against_lhp)
 
     # Yay more dataframes 
     ops_against_rhp_df = ops_against_rhp.to_frame(name='Value')
@@ -139,12 +139,12 @@ def main():
     # # #
 
 	# Now we can calculate a team's BA against righties
-    team_avg_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['H'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['AB'].sum())).round(3)
+    team_avg_against_rhp = (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['H'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['AB'].sum())
 
     # And against southpaws
-    team_avg_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['H'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['AB'].sum())).round(3)
+    team_avg_against_lhp = (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['H'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['AB'].sum())
 
     # Create dataframes
     team_avg_against_rhp_df = team_avg_against_rhp.to_frame(name='Value')
@@ -161,22 +161,22 @@ def main():
 
 
     # Do the wall again for team OBP
-    team_obp_against_rhp = (((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['H'].sum()) \
+    team_obp_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['H'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['AB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['SF'].sum()))
 
     # Now for a team's OBP against lefties
-    team_obp_against_lhp = (((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['H'].sum()) \
+    team_obp_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['H'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['AB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['SF'].sum()))
 
     # Dataframes
     team_obp_against_rhp_df = team_obp_against_rhp.to_frame(name='Value')
@@ -193,12 +193,12 @@ def main():
 
 
     # Calculate team slugging percentage against rhp 
-    team_slg_against_rhp = ((raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['TB'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['AB'].sum())).round(3)
+    team_slg_against_rhp = (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['TB'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='R'].groupby('HitterTeamId')['AB'].sum())
 
     # Calculate team slugging percentage against lhp 
-    team_slg_against_lhp = ((raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['TB'].sum()) \
-    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['AB'].sum())).round(3)
+    team_slg_against_lhp = (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['TB'].sum()) \
+    	/ (raw_df[raw_df['PitcherSide']=='L'].groupby('HitterTeamId')['AB'].sum())
 
     # Team slugging dataframes
     team_slg_against_rhp_df = team_slg_against_rhp.to_frame(name='Value')
@@ -215,10 +215,10 @@ def main():
 
 
     # End with team ops
-    team_ops_against_rhp = (team_obp_against_rhp + team_slg_against_rhp).round(3)
+    team_ops_against_rhp = (team_obp_against_rhp + team_slg_against_rhp)
 
     # Now against lefties
-    team_ops_against_lhp = (team_obp_against_lhp + team_slg_against_lhp).round(3)
+    team_ops_against_lhp = (team_obp_against_lhp + team_slg_against_lhp)
 
     # last set of batting dfs
     team_ops_against_rhp_df = team_ops_against_rhp.to_frame(name='Value')
@@ -239,12 +239,12 @@ def main():
     # # # 
 
     # Let's look at a pitcher's BA against vs right handed hitters
-    avg_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['H'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['AB'].sum())).round(3)
+    avg_against_rhh = (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['H'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['AB'].sum())
 
     # Now let's see pitching against lefties
-    avg_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['H'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['AB'].sum())).round(3)
+    avg_against_lhh = (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['H'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['AB'].sum())
 
     # Generate first of the pitching dataframes
     avg_against_rhh_df = avg_against_rhh.to_frame(name='Value')
@@ -261,22 +261,22 @@ def main():
 
 
     # Find each pitcher's OBP against right handers
-    obp_against_rhh = (((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['H'].sum()) \
+    obp_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['H'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['AB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['SF'].sum()))
 
     # And for a pitcher's OBP against lefties
-    obp_against_lhh = (((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['H'].sum()) \
+    obp_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['H'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['AB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['SF'].sum()))
 
     # Dataframes for obp against
     obp_against_rhh_df = obp_against_rhh.to_frame(name='Value')
@@ -293,12 +293,12 @@ def main():
     
 
     # How about slugging percentage against rhh 
-    slg_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['TB'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['AB'].sum())).round(3)
+    slg_against_rhh = (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['TB'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherId')['AB'].sum())
 
     # Then slugging percentage against lhh 
-    slg_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['TB'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['AB'].sum())).round(3)
+    slg_against_lhh = (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['TB'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherId')['AB'].sum())
 
     # Dataframes
     slg_against_rhh_df = slg_against_rhh.to_frame(name='Value')
@@ -315,10 +315,10 @@ def main():
 
 
     # Calculate OPS against for right handed batters
-    ops_against_rhh = (obp_against_rhh + slg_against_rhh).round(3)
+    ops_against_rhh = (obp_against_rhh + slg_against_rhh)
 
     # Now against lefties
-    ops_against_lhh = (obp_against_lhh + slg_against_lhh).round(3)
+    ops_against_lhh = (obp_against_lhh + slg_against_lhh)
     # Whoever 656546 is has some really solid stuff 
     # last dataframes
     ops_against_rhh_df = ops_against_rhh.to_frame(name='Value')
@@ -339,12 +339,12 @@ def main():
     # # #
 
 	# Now we can calculate a pitching staff's BA against for righties
-    team_avg_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['H'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['AB'].sum())).round(3)
+    team_avg_against_rhh = (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['H'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['AB'].sum())
 
     # Against lefties
-    team_avg_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['H'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['AB'].sum())).round(3)
+    team_avg_against_lhh = (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['H'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['AB'].sum())
 
     # Last set of data frames
     team_avg_against_rhh_df = team_avg_against_rhh.to_frame(name='Value')
@@ -361,21 +361,21 @@ def main():
 
 
     # Do the wall one last time for team OBP against
-    team_obp_against_rhh = (((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['H'].sum()) \
+    team_obp_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['H'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['AB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['SF'].sum()))
 
-    team_obp_against_lhh = (((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['H'].sum()) \
+    team_obp_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['H'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['HBP'].sum())) \
     	/ ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['AB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['BB'].sum()) \
     	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['HBP'].sum()) \
-    	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['SF'].sum()))).round(3)
+    	+ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['SF'].sum()))
 
     # Dataframes
     team_obp_against_rhh_df = team_obp_against_rhh.to_frame(name='Value')
@@ -392,12 +392,12 @@ def main():
 
 
     # Calculate team slugging percentage vs rhh
-    team_slg_against_rhh = ((raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['TB'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['AB'].sum())).round(3)
+    team_slg_against_rhh = (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['TB'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='R'].groupby('PitcherTeamId')['AB'].sum())
 
     # Finally end with team slugging percentage against vs lhh 
-    team_slg_against_lhh = ((raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['TB'].sum()) \
-    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['AB'].sum())).round(3)
+    team_slg_against_lhh = (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['TB'].sum()) \
+    	/ (raw_df[raw_df['HitterSide']=='L'].groupby('PitcherTeamId')['AB'].sum())
 
     # I think you know these are the dataframes by now
     team_slg_against_rhh_df = team_slg_against_rhh.to_frame(name='Value')
@@ -414,10 +414,10 @@ def main():
 
 
     # End with team ops
-    team_ops_against_rhh = (team_obp_against_rhh + team_slg_against_rhh).round(3)
+    team_ops_against_rhh = (team_obp_against_rhh + team_slg_against_rhh)
 
     # Now against lefties
-    team_ops_against_lhh = (team_obp_against_lhh + team_slg_against_lhh).round(3)
+    team_ops_against_lhh = (team_obp_against_lhh + team_slg_against_lhh)
 
     # Final set of individual dataframes
     team_ops_against_rhh_df = team_ops_against_rhh.to_frame(name='Value')
@@ -530,6 +530,9 @@ def main():
 
     # Reorganize the order of the columns
     aggregate_df = aggregate_df[['SubjectId', 'Stat', 'Split', 'Subject', 'Value']]
+
+    # Round the Values to the nearest thousandth
+    aggregate_df = aggregate_df.round(3)
 
     # Now sort over first four columns
     sorted_aggregate_df = aggregate_df.sort_values(by=['SubjectId','Stat','Split','Subject'])
